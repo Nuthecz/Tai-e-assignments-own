6bd801d91257cd07c4b2b688146fb8fb72c8cc65
Nuthecz <2973174592@qq.com>
2023-11-21 22:06:28 +0800

    A4: Implement class hierarchy analysis

--- A4/tai-e/src/main/java/pascal/taie/analysis/graph/callgraph/CHABuilder.java
+++ A4/tai-e/src/main/java/pascal/taie/analysis/graph/callgraph/CHABuilder.java
@@ -31,6 +31,7 @@ import pascal.taie.language.classes.JMethod;
 import pascal.taie.language.classes.Subsignature;
 
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.Queue;
 import java.util.Set;
 
@@ -51,6 +52,22 @@ class CHABuilder implements CGBuilder<Invoke, JMethod> {
         DefaultCallGraph callGraph = new DefaultCallGraph();
         callGraph.addEntryMethod(entry);
         // TODO - finish me
+        Queue<JMethod> WorkList = new ArrayDeque<>();
+        WorkList.add(entry);
+        while(!WorkList.isEmpty()){
+            JMethod method = WorkList.poll();
+            // 添加为可达方法
+            if(!callGraph.reachableMethods.contains(method))
+                callGraph.addReachableMethod(method);
+            // 遍历该方法的调用语句(通过方法中调用点来获取)，将调用的方法加入到队列中
+            for(Invoke callSite: callGraph.getCallSitesIn(method)){
+                Set<JMethod> possibleCallees = resolve(callSite);
+                for(JMethod callee : possibleCallees){
+                    callGraph.addEdge(new Edge<>(CallGraphs.getCallKind(callSite), callSite, callee));
+                    WorkList.add(callee);
+                }
+            }
+        }
         return callGraph;
     }
 
@@ -58,8 +75,30 @@ class CHABuilder implements CGBuilder<Invoke, JMethod> {
      * Resolves call targets (callees) of a call site via CHA.
      */
     private Set<JMethod> resolve(Invoke callSite) {
-        // TODO - finish me
-        return null;
+        /* TODO - finish me */
+        // 维护结果集，就是Resolve中的 T
+        Set<JMethod> possibleTarGet = new HashSet<>();
+        MethodRef methodRef = callSite.getMethodRef();
+        switch (CallGraphs.getCallKind(callSite)){
+            case STATIC:
+            case SPECIAL:// 调用静态方法或者私有方法，直接dispathc寻找对应的方法
+                possibleTarGet.add(dispatch(methodRef.getDeclaringClass(),methodRef.getSubsignature()));
+                break;
+            case VIRTUAL:
+            case INTERFACE:// 调用虚方法或者接口方法，需要根据继承关系进行查找
+                Queue<JClass> jClassQueue = new ArrayDeque<>();
+                jClassQueue.add(methodRef.getDeclaringClass());
+                while(!jClassQueue.isEmpty()){
+                    JClass jClass = jClassQueue.poll();
+                    JMethod method = dispatch(jClass, methodRef.getSubsignature());
+                    // 如果找到了对应的方法，就将其加入到结果集中
+                    if(method != null)
+                        possibleTarGet.add(method);
+                    // 将该类的子类加入到队列中
+                    jClassQueue.addAll(hierarchy.getDirectSubclassesOf(jClass));
+                }
+        }
+        return possibleTarGet;
     }
 
     /**
@@ -69,7 +108,16 @@ class CHABuilder implements CGBuilder<Invoke, JMethod> {
      * can be found.
      */
     private JMethod dispatch(JClass jclass, Subsignature subsignature) {
-        // TODO - finish me
+        /* TODO - finish me */
+        // 根据子签名返回该类中声明的对应方法，这是最后的递归出口
+        JMethod method =  jclass.getDeclaredMethod(subsignature);
+        if(method != null) return method;
+
+        // 根据签名递归到父类中查找对应方法
+        JClass jClass = jclass.getSuperClass();
+        if(jClass != null) return dispatch(jClass, subsignature);
+
+        // 其余情况返回null
         return null;
     }
 }
